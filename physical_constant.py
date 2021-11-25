1798993d829e2108b22ccf6fc5f155ddff498ef8
ElinaElissa <92169216+ElinaElissa@users.noreply.github.com>
2021-11-25 21:23:38 +0000

    less_3

---
+++ physical_constant.py
@@ -0,0 +1,7 @@
+gavity_constant = 6.67 * 10**(-11)
+proton_mass = 1.67 * 10**(-27)
+neitron_mass = 1.675 * 10**(-27)
+electron_mass = 9.11 * 10**(-31)
+avogadro_constant = 6.02 * 10**(-23)
+boltzmann_constant = 1.38 * 10**(-23)
+faradei_constant = 96484.56
